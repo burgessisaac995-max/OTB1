2c257d6bfe05c6bcffa95c7e5cfa268bb873aabd
isaac <burgessisaac995@gmail.com>
2026-09-24 19:30:17 +0000

    folders and files in the repository made by copilot and the user. The user has made changes to the paylowd/payloudhelper.asm file and added a new file paylowd/payloudhelper. The commit message is currently empty, and the user is prompted to enter a commit message for their changes. Lines starting with '#' will be ignored, and an empty message will abort the commit. The user is on the 'main' branch, which has diverged from 'origin/main', with each branch having one different commit.

--- paylowd/payloudhelper.asm
+++ paylowd/payloudhelper.asm
@@ -4,17 +4,50 @@
 
 .section .text
 _start:
-	mov rax, [rsp]
-	lea rsi, [rsp + 8]
-	lea rdx, [rsi + rax * 8 + 8]
-	lea rdi, [rip + payloud_path]
+	mov r12, [rsp]
+	lea r13, [rsp + 8]
+	sub rsp, 4096
+
+	lea rdi, [rip + proc_exe]
+	mov rsi, rsp
+	mov rdx, 4095
+	mov rax, 89
+	syscall
+	test rax, rax
+	js launch_failed
+
+	mov r14, rax
+find_directory_end:
+	dec r14
+	cmp byte ptr [rsp + r14], '/'
+	jne find_directory_end
+
+	lea r8, [rip + payload_suffix]
+	lea r9, [rsp + r14]
+copy_payload_path:
+	mov al, byte ptr [r8]
+	mov byte ptr [r9], al
+	inc r8
+	inc r9
+	test al, al
+	jnz copy_payload_path
+
+	mov rax, r12
+	inc rax
+	shl rax, 3
+	lea rdx, [r13 + rax]
+	lea rsi, [r13]
+	mov rdi, rsp
 	mov rax, 59
 	syscall
 
+	launch_failed:
 	mov rax, 60
 	mov rdi, 127
 	syscall
 
 .section .rodata
-payloud_path:
-	.asciz "./payloud.bin"
+proc_exe:
+	.asciz "/proc/self/exe"
+payload_suffix:
+	.asciz "/payloud.bin"
